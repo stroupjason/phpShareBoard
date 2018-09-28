7095a7392a6b81f53311d53879e8c37edf551b17
Jason Stroup <jasonstroup12@gmail.com>
2018-09-28 13:23:54 -0600

    finished classes & methods

--- playground/OOPfundamentals/classesMethods.php
+++ playground/OOPfundamentals/classesMethods.php
@@ -69,18 +69,101 @@
     // $User = new User;
     // $User->$login(); //error: recreated issue 'method name must be a string in' //error because login does not need a $ in front
 
-     Class User{
-        public function register(){
-            echo 'User Registered';
-        }
-        public function login($username, $password){
-            echo $username.' is now logged in.';
-        }
+    //  Class User{
+    //     public function register(){
+    //         echo 'User Registered';
+    //     }
+    //     public function login($username, $password){
+    //         echo $username.' is now logged in.';
+    //     }
+    // }
+    // $User = new User;
+    // $User->login('Jason', '12345'); 
+
+    //now do it again
+
+    // Class User {
+    //     public function register(){
+    //         echo 'User Registered';
+    //     }
+    //     public function login($username, $password){
+    //         echo $username.' has succesfully logged in.';
+    //     }
+    // }
+
+    // $User = new User;
+    // $User->login('Jason'); //if you don't call the second argument you get this error: 'Warning: Missing argument 2 for User::login(), called in' (so just add 'anything here' as the second parameter) 
+
+//CHALLENGE : create a constructor function (hint: to call this function you do need to call anything at the bottom, so make sure there are no methods being called)
+
+// class User{
+//     public function __construct(){   //make sure to spell everything correctly! *construct
+//         echo 'Constructor Executed';
+//     }
+//     public function register(){
+//         echo 'User Registered';
+//     }
+//     public function login($username, $password){
+//         echo $username.' is now logged in.';
+//     }
+//    public function __destruct(){ //close database connections or run at the end of any scripts
+//         echo 'Destructor Executed';
+//     }
+// }
+
+//     $User = new User;
+
+
+//CHALENGE: CALL A METHOD WITHIN ANOTEHR METHOD (hint: use 'this' keyword)
+
+// class User {
+//     public function __construct(){
+//         echo ' contstructor executed.';
+//     }
+//     public function register(){
+//         echo ' User Registered.'; 
+//     }
+//     public function login($username, $password){
+//         echo $username.' logged in successfully.';
+//     }
+//     public function auth_user($username, $password){
+//         echo $username.' is authenticated.';
+//     }
+//     public function __destruct(){
+//         echo ' destructor executed.';
+//     }
+
+// }
+
+// $User = new User;
+// $User->register();
+
+//this code doesn't work becuase it doesn't bind "this keyword" when logging in to check for authentication
+
+class User {
+    public function __construct(){
+        echo ' contstructor executed.';
     }
-    $User = new User;
-    $User->login('Jason', '12345'); //recreate issue 'method name must be a string in'
+    public function register(){
+        echo ' User Registered.'; 
+    }
+    public function login($username, $password){
+        $this->auth_user($username, $password);
+    }
+    public function auth_user($username, $password){
+        echo $username.' is authenticated.';
+    }
+    public function __destruct(){
+        echo ' destructor executed.';
+    }
+
+}
+
+$User = new User;
+$User->login(' Jason', '1234');  //calling auth user method from login method
 
 ?>
+
 
 
 
